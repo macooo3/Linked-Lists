8e8b429ea654c6a22661cb4e67433afb17b54b35
macooo3 <marcoysu765@gmail.com>
2024-11-28 10:37:25 -0500

    added new testing branch/ try to add new value

--- node.js
+++
@@ -1,6 +0,0 @@
-export default class Node {
-   constructor(value = null, nextNode){
-    this.value = value;
-    this.nextNode = nextNode;
-   }
-}
--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-import Node from "./node.js";
+// import Node from "./node.js";
+class Node {
+  constructor(value = null, nextNode = null){
+   this.value = value;
+   this.nextNode = nextNode;
+  }
+}
 
 class LinkedList extends Node {
   constructor(value, nextNode) {
     super(value, nextNode);
   }
   append(value){
-    this.value = {head:{
+    if(this.value === null)
+    {this.value = {head:{
       value:value,
-
-    }}
+      next: this.nextNode
+    }}}
+   
+    {
+      this.value.head.next = {
+        value:value,
+        next: this.nextNode
+      }
+    }
+    return this.value
   }
   
 }
+const list = new LinkedList()
+
+list.append('string')
+list.append('string2')
+
+console.log(list.append('string3'))
 
 
 
