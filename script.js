7c6be1ef601a1bbb7dd5dde8f22178f10267add9
macooo3 <marcoysu765@gmail.com>
2024-12-02 17:48:21 -0500

    finalize

--- script.js
+++ script.js
@@ -10,17 +10,15 @@ class LinkedList extends Node {
   constructor(value, nextNode) {
     super(value, nextNode);
     this.head = null;
-    this.length = 0;
   }
   append(value) {
     const node = new Node(value);
-
     // checks if this.head = null sets new node to head
     // else will set current head to current
     // skip while loop cause head nextNode does equal null so we set the nextNode to new node
     // in the loop we set current to the nextNode after will be null so we skip loop
     // then set newNode to nextNode
-    if (this.head === null) this.head = node;
+    if (!this.head) this.head = node;
     else {
       let current = this.head;
       while (current.nextNode != null) {
@@ -28,14 +26,117 @@ class LinkedList extends Node {
       }
       current.nextNode = node;
     }
-    this.length++;
+
+    return this.head;
   }
-  prepend(value){
-    
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.head === null) this.head = node;
+    else {
+      let extend = this.head;
+      this.head = node;
+      this.head.nextNode = extend;
+    }
+    console.log(this.head);
+  }
+
+  size() {
+    let count = 1;
+    if (this.head === null) return 0;
+    else {
+      let current = this.head;
+      while (current.nextNode != null) {
+        count++;
+        current = current.nextNode;
+      }
+    }
+    return count;
+  }
+  head() {
+    return this.head;
+  }
+
+  tail() {
+    if (this.head.nextNode === null) return this.head;
+    else {
+      let current = this.head;
+      while (current.nextNode != null) {
+        current = current.nextNode;
+      }
+      console.log(current);
+    }
+  }
+
+  at(index) {
+    let count = 1;
+    if (index == count) {
+      return this.head;
+    } else {
+      let current = this.head;
+      while (count != index) {
+        current = current.nextNode;
+        count++;
+      }
+      return current;
+    }
+  }
+  pop() {
+    let current = this.head;
+    let prev;
+    while (current.nextNode != null) {
+      prev = current;
+      current = current.nextNode;
+    }
+    prev.nextNode = null;
+    return this.head;
+  }
+  contains(value) {
+    let current = this.head;
+    while (current != null) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.nextNode;
+    }
+    return false;
+  }
+  find(value) {
+    let current = this.head;
+    let count = 1;
+    while (current != null) {
+      if (current.value === value) {
+        return count;
+      }
+      current = current.nextNode;
+      count++;
+    }
+    return false;
+  }
+  toString(){
+    let current = this.head;
+    let string =''
+    while (current !=null){
+      if(current.value){
+        string +=`(${current.value}) -> `
+      }
+      current = current.nextNode;
+    }
+    return string += 'null'
   }
 }
-const list = new LinkedList(); 
+const list = new LinkedList();
 
-
-
-
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+// list.tail();
+// console.log(list.size());
+// console.log(list.at(4));
+// console.log(list.pop());
+console.log(list.contains("turtle"));
+console.log(list.find("turtle"));
+console.log(list.toString());
